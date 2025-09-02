874b4a98f49a5505c1e2c3fdee92fdbfa5fb8b8f
Fristaylo <andru_2004@mail.ru>
2025-09-03 02:15:09 +0400

    Fix? #3

--- server/index.js
+++ server/index.js
@@ -378,12 +378,6 @@ const httpCheckAndSave = async () => {
 // Serve static files from the React app after API routes
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
   createTable();
