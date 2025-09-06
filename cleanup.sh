70746084b214059a6f9603354ec9d92bdb55758d
Fristaylo <andru_2004@mail.ru>
2025-09-06 23:03:53 +0400

    Fix readme / Delete unused scripts

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Connecting to the database and clearing ping_logs table..."
-docker-compose exec db psql -U uptime_user -d uptime_checker -c "TRUNCATE TABLE ping_logs;"
-echo "Table ping_logs has been cleared."
--- init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER uptime_user WITH PASSWORD 'password';
-    CREATE DATABASE uptime_checker;
-    GRANT ALL PRIVILEGES ON DATABASE uptime_checker TO uptime_user;
-EOSQL
